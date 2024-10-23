b118dbcffc11b9e5c38abfb1bedfc96c21b27bbb
JDVega6 <juanguativa07@gmail.com>
2024-10-23 17:02:45 -0500

    Change: Replaced Azure Service Bus with RabbitMQ

--- Galaxi.Movie.API/Program.cs
+++ Galaxi.Movie.API/Program.cs
@@ -62,9 +62,13 @@ builder.Services.AddMassTransit(x =>
     x.AddConsumer<AddMovieConsumer>();
     x.AddConsumer<DeleteMovieConsumer>();
 
-    x.UsingAzureServiceBus((context, cfg) =>
+    x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.Host(configuration.GetConnectionString("AzureServiceBus"));
+        cfg.Host(configuration.GetConnectionString("rabbitMqSettingsHost"), h =>
+        {
+            h.Username(configuration.GetConnectionString("rabbitMqSettingsUsername"));
+            h.Password(configuration.GetConnectionString("rabbitMqSettingsPassword"));
+        });
 
         cfg.ConfigureEndpoints(context);
     });
